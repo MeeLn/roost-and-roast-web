cdaa4ff44cb89b2c1ab936eb36a041c97869242d
Rajesh Shrestha <rajeshrestha@gmail.com>
2026-02-15 12:46:38 +0545

    feat: comment out Testimonials section in the homepage layout.

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <GridMenuSection />
       <MarqueeSection />
       <AboutStory />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <CTASection />
       <ContactSection />
       <Footer />
